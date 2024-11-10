3a6f62437de4187b7b3da94c22149514e81972d3
Francis_ldn <explorer18@protonmail.com>
2024-11-10 14:41:28 +0000

    edit route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -14,9 +14,11 @@ export async function POST(req: Request) {
       'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
     );
   }
-
   // Get the headers
   const headerPayload = await headers();
+  headerPayload.set('Access-Control-Allow-Origin', '*');
+  headerPayload.set('Access-Control-Allow-Methods', 'POST');
+  headerPayload.set('Access-Control-Allow-Headers', 'Content-Type');
   const svix_id = headerPayload.get('svix-id');
   const svix_timestamp = headerPayload.get('svix-timestamp');
   const svix_signature = headerPayload.get('svix-signature');
